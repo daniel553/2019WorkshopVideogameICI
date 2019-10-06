de409c8b7f55400c4d4fcf7b9f46d34987b01a50
daniel <daniel553@github.com>
2019-10-06 10:17:03 -0500

    [Cleaned] assets and removed for empty project

--- sketch.js
+++ sketch.js
@@ -1,27 +1,11 @@
 //VYC
-let sonic;
-let rings;
 
-/**
- * Used to preload assets
- */
 function preload() {
-    sonic = loadImage('./assets/sonic.png');
-    soundFormats('wav');
-    rings = loadSound('assets/rings.wav');
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
 }
 
 function draw() {
-    //Paint the sonic
-    image(sonic, 100, 100, 200, 300);
-}
-
-function mousePressed(){
-    //Play a clip sound
-    rings.setVolume(0.5);
-    rings.play();
 }
