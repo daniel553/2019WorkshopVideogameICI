f3c93362a6cbec952834d1d54e54fdbdc69a9924
daniel <daniel553@github.com>
2019-10-05 23:05:25 -0500

    [Added] simple but most used events

--- sketch.js
+++ sketch.js
@@ -1,21 +1,58 @@
+//VYC
+let colorKeyPressed = 'white'
+let mouseToggle = true;
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
-    background(0);
-    //Basic Shapes
-    //Line line(x1, y1, x2, y2)
-    stroke(255);
-    strokeWeight(5);
-    line(30, 20, 85, 75);
-    //Rectangle  rect(x, y, w, h, [tl], [tr], [br], [bl])
-    rect(90, 20, 55, 55);
-    //Circle circle(x, y, d)
-    circle(190,50,50);
-    //Triangle triangle(x1, y1, x2, y2, x3, y3)
-    triangle(30, 175, 58, 120, 86, 175);
-    //Curves curve(x1, y1, x2, y2, x3, y3, x4, y4)
-    curve(5, 5, 100, 120, 170, 120, 270, 260);
+    //If mouse pressed do not refresh background (resets)
+    if(mouseToggle){
+        background(0);
+    }
+    //Set a color depending on key pressed
+    fill(colorKeyPressed);
+    //Draw a circle where mouse is
+    circle(mouseX,mouseY,50);
 
 }
+
+/**
+ * Everytime you type a keyboard, this function is called automatically
+ */
+function keyTyped(){
+    switch (key) {
+        case 'r':
+            colorKeyPressed = 'red';
+            break;
+        case 'g':
+            colorKeyPressed = 'green';
+            break;
+        case 'b':
+            colorKeyPressed = 'blue';
+            break;
+        default:
+            colorKeyPressed = 'white';
+    }
+}
+
+/**
+ * If left arrow is pressed then we pause the program, and right arrow resumes it.
+ */
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        noLoop();
+    } else if (keyCode === RIGHT_ARROW) {
+        loop();
+    }
+}
+
+/**
+ * Toggles mouse if pressed = true, false otherwise.
+ */
+function mousePressed() {
+    mouseToggle = !mouseToggle;
+    // prevent default
+    return false;
+  }
