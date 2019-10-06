6017152118309a3656d7a36284ed91a09de0b931
daniel <daniel553@github.com>
2019-10-06 11:13:48 -0500

    [Added] enemy and hero with controls

--- flyguy.js
+++ flyguy.js
@@ -1,8 +1,9 @@
 class FlyGuy {
-    constructor(x, y, color) {
+    constructor(x, y, color, isControlled) {
         this.x = x;
         this.y = y;
         this.color = color;
+        this.isControlled = isControlled;
         this.w = 50;
         this.h = 50;
         this.vx = 0;
@@ -18,29 +19,31 @@ class FlyGuy {
         noStroke();
         rect(this.x, this.y, this.w, this.h);
 
-        if(this.ballon1){
+        if (this.ballon1) {
             this.ballon1.updatePos(this);
             this.ballon1.show();
         }
-        if(this.ballon2){
+        if (this.ballon2) {
             this.ballon2.updatePos(this);
             this.ballon2.show();
         }
     }
 
     move() {
-        //Apply forces for
-        //Up
-        if (keyIsDown(UP_ARROW)) {
-            this.vy = -6;
-        }
-        //left
-        if (keyIsDown(LEFT_ARROW)) {
-            this.vx = this.vx - 2;
-        }
-        //right
-        if (keyIsDown(RIGHT_ARROW)) {
-            this.vx = this.vx + 2;
+        if (this.isControlled) {
+            //Apply forces for
+            //Up
+            if (keyIsDown(UP_ARROW)) {
+                this.vy = -6;
+            }
+            //left
+            if (keyIsDown(LEFT_ARROW)) {
+                this.vx = this.vx - 2;
+            }
+            //right
+            if (keyIsDown(RIGHT_ARROW)) {
+                this.vx = this.vx + 2;
+            }
         }
 
         //Apply gravity
@@ -51,15 +54,15 @@ class FlyGuy {
 
         //Right and left.
         if (this.vx != 0) {
-            if (this.vx > 0){
+            if (this.vx > 0) {
                 this.vx = Math.round(this.vx - 1);
-                if(this.vx < -3)
-                    this.vx = -3;
+                if (this.vx < -2)
+                    this.vx = -2;
             }
-            if (this.vx < 0){
+            if (this.vx < 0) {
                 this.vx = Math.round(this.vx + 1);
-                if(this.vx > 3)
-                this.vx = 3;
+                if (this.vx > 2)
+                    this.vx = 2;
             }
 
         }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 //VYC
 let flyGuy;
+let enemy;
 
 function preload() {
 }
 
 function setup() {
     createCanvas(600, 600);
-    flyGuy = new FlyGuy(100,100, 'red');
+    flyGuy = new FlyGuy(100,100, 'red', true);
     flyGuy.ballon1 = new Ballon(flyGuy.x, flyGuy.y, -15,-30, 'blue');
     flyGuy.ballon2 = new Ballon(flyGuy.x, flyGuy.y, 30,-30, 'blue');
+    enemy = new FlyGuy(300,300, 'green');
+    enemy.ballon1 = new Ballon(enemy.x, enemy.y, 15, -30, 'pink');
 }
 
 function draw() {
@@ -17,4 +20,8 @@ function draw() {
     //Show hero
     flyGuy.move();
     flyGuy.show();
+
+    //Show enemy
+    enemy.move();
+    enemy.show();
 }
